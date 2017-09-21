f03a593f4a2f1683fe512722076c736935aabbe5
Yonatan Ben Avraham <yonbav@gmail.com>
2017-09-21 15:03:30 +0300

    adding an hello world app

---
+++ hello world2/hello world2/hello world2.cpp
@@ -0,0 +1,18 @@
+// hello world2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world \n";
+	int x;
+	cin >> x;
+    return 0;
+}
+
